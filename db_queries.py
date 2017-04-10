cb9197858084b0e7ffffb2315c7e2b73a558755a
Mohit Sharma <iammohitsharma@gmail.com>
2017-04-10 23:07:15 +0530

    movies queries done

--- db_queries.py
+++ db_queries.py
@@ -8,7 +8,7 @@ class Database():
 		self.conn = sql.connect()
 		self.cursor = self.conn.cursor()
 
-
+	# Done
 	def auth_user(self, id, pwd):
 		self.cursor.execute("SELECT type FROM user WHERE email='%s' AND pass='%s'" % (id, pwd))
 		res = self.cursor.fetchall()
@@ -18,6 +18,7 @@ class Database():
 			return None
 		return type
 
+	# Done
 	def get_halls(self):
 		self.cursor.execute("select * from hall")
 		res = self.cursor.fetchall()
@@ -32,6 +33,7 @@ class Database():
 			json.append(x)
 		return json
 
+	# Done
 	def add_hall(self, h_name, platinum, gold, silver):
 		i = 1
 		self.cursor.execute("INSERT INTO hall(h_name) VALUES('%s')" % h_name)
@@ -47,49 +49,97 @@ class Database():
 			i += 1
 		self.conn.commit()
 
+	# Done
 	def delete_hall(self, hid):
 		hid = str(hid)
 		self.cursor.execute("DELETE FROM hall WHERE hid=%s" % (hid))
 		self.conn.commit()
 
+	# Done
 	def get_movies(self):
 		self.cursor.execute("select * from movie")
 		res = self.cursor.fetchall()
 		json = []
 		for (mid, title, rating, descr, img, lang) in res:
+			self.cursor.execute("SELECT genre FROM movie_genre WHERE mid = %d"%mid)
+			genres = self.cursor.fetchall()
+			g = []
+			for x in list(genres):
+				g.append(x[0])
 			x = {
 				"mid": mid,
 				"title": title,
 				"rating": rating,
 				"descr": descr,
 				"img": img,
-				"lang": lang
+				"lang": lang,
+				"genre": g
 			}
 			json.append(x)
 		return str(json)
 
-	def add_movies(self, title, descr, rating, lang, img):
-		query = "INSERT INTO movie(title, rating, descr, img, lang) VALUES('%s','%s','%s','%s','%s')" % (title, rating, descr, img, lang)
+	# Done
+	def get_movie_search(self, name):
+		self.cursor.execute("SELECT * FROM movie WHERE title='%s'" % name)
+		res = self.cursor.fetchall()
+		json = []
+		for (mid, title, rating, descr, img, lang) in res:
+			self.cursor.execute("SELECT genre FROM movie_genre WHERE mid = %d"%mid)
+			genres = self.cursor.fetchall()
+			g = []
+			for x in list(genres):
+				g.append(x[0])
+			x = {
+				"mid": mid,
+				"title": title,
+				"rating": rating,
+				"descr": descr,
+				"img": img,
+				"lang": lang,
+				"genre": g
+			}
+			json.append(x)
+		return json
+
+	# Done
+	def add_movie(self, title, descr, rating, lang, img, genre):
+		query = "INSERT INTO movie(title, rating, descr, img, lang) VALUES('%s', '%s', '%s', '%s', '%s')" % (title, rating, descr, img, lang)
+		print(query)
 		self.cursor.execute(query)
+		pk = self.cursor.lastrowid
+		for g in genre.split(','):
+			g = g.strip()
+			self.cursor.execute("INSERT INTO movie_genre VALUES(%d, '%s')" % (pk, g))
 		self.conn.commit()
 
+	# Done
 	def delete_movie(self, mid):
 		mid = str(mid)
 		self.cursor.execute("DELETE FROM movie WHERE mid=%s" % (mid))
+		self.conn.commit()
 
+
+	# Done
 	def get_shows(self):
-		self.cursor.execute("select * from shows")
+		self.cursor.execute("SELECT * FROM hall NATURAL JOIN shows NATURAL JOIN movie")
 		res = self.cursor.fetchall()
 		json = []
-		for (hid,mid,time,avail) in res:
+		for (hid,mid,h_name,n_seats,time,avail,title,rating,descr,img,lang) in res:
 			x = {
 				"mid": mid,
 				"hid": hid,
+				"h_name": h_name,
+				"n_seats": n_seats,
 				"time": time,
 				"avail": avail,
+				"title": title,
+				"rating": rating,
+				"descr": descr,
+				"img": img,
+				"lang":lang
 			}
 			json.append(x)
-		return str(json)
+		return json
 
 	# def book_show(self,hid,sid_list):
 	# 	for sid in sid_list:
