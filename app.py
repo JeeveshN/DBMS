6492fba0787e096c385b5f050fbd27a484c7b526
naman1901 <naman1901@gmail.com>
2017-04-10 20:43:08 +0530

    index

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
 from flask import Flask,render_template,redirect,url_for,request,flash,session
 
-
 app = Flask(__name__)
 
+def authorize(u,p):
+	return None
+
 @app.route('/')
-@app.route('/logged_in',method=['POST','GET'])
-def logged_in():
+def index():
+	return render_template('index.html',incorrect='True')
 
-	if 'user' in session:
-		return render_template('')
-	if request.method == 'POST':
-		""" Implement user login functionality """
+@app.route('/login/submit', methods=['POST'])
+def loginSub():
+	user = request.form['email']
+	password = request.form['password']
+	auth = authorize(userid,password)
+	print auth
+	# if auth is not None:
+	# 	session.push('user',auth)
+	# 	if(auth=='user'):
+	# 		return render_template
+	# 	else:
+	# 		return render_template
+	# else:
+	# 	return render_template('index.html',incorrect='True')
+	return ('OK')
+# @app.route('/change_show',method=['POST'])
+# def change_show():
 
 
-	return render_template('login.html')
+# @app.route('/change_movie',method=['POST'])
+# def change_movie():
 
-@app.route('/change_show',method=['POST'])
-def change_show():
+# @app.route('/all_movies')
+# def all_movies():
 
-
-@app.route('/change_movie',method=['POST'])
-def change_movie():
-
-@app.route('/all_movies')
-def all_movies():
-
-@app.route('/logout')
-def logout():
-	session.pop('user',None)
-	return redirect(url_for('logged_in'))
+# @app.route('/logout')
+# def logout():
+# 	session.pop('user',None)
+# 	return redirect(url_for('logged_in'))
 
 
 
