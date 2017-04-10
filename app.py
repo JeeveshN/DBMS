6796e49362f3715c19fd80e5b67e90df90f3c183
naman1901 <naman1901@gmail.com>
2017-04-11 01:53:38 +0530

    users 101

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from db_queries import Database
 app = Flask(__name__)
 app.secret_key = 'DBMS'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'ms101234321'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'N@1901root'
 app.config['MYSQL_DATABASE_DB'] = 'project'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 db = Database(app)
@@ -64,25 +64,12 @@ def rem_show():
 
 @app.route('/addhall')
 def add_hall():
-<<<<<<< HEAD
 	return render_template('admin/addhall.html')
 
 @app.route('/remhall')
 def rem_hall():
-	return render_template('admin/remhall.html')
-=======
-	db.add_hall('Audi 5', 10, 15, 25)
-	return ('Add Hall')
-
-@app.route('/remhall')
-def rem_hall():
-	db.delete_hall(8)
-	return ('Remove Show')
->>>>>>> cb9197858084b0e7ffffb2315c7e2b73a558755a
-
-@app.route('/test')
-def test():
-	return db.get_halls()
+	halls = db.get_halls()
+	return render_template('admin/remhall.html', halls=halls)
 
 @app.route('/addmovie')
 def add_movie():
@@ -90,7 +77,8 @@ def add_movie():
 
 @app.route('/remmovie')
 def rem_movie():
-	return render_template('admin/remmovie.html')
+	movies = db.get_movies()
+	return render_template('admin/remmovie.html', movies=movies)
 
 # @app.route('/addshow/submit',methods=['POST'])
 # def add_show_sub():
@@ -107,8 +95,11 @@ def add_hall_sub():
 	db.add_hall(hname,n_p,n_g,n_s)
 	return redirect('admin')
 
-# @app.route('/remhall/submit',methods=['POST'])
-# def rem_hallSub():
+@app.route('/remhall/submit',methods=['POST'])
+def rem_hallSub():
+	bid = request.form['del']
+	db.delete_halls(bid)
+	return redirect('admin')
 
 @app.route('/addmovie/submit',methods=['POST'])
 def add_movieSub():
@@ -124,24 +115,39 @@ def add_movieSub():
 # @app.route('/remmovie/submit',methods=['POST'])
 # def rem_movieSub():
 
-# @app.route('/change_show',method=['POST'])
-# def change_show():
+@app.route('/viewmovie')
+def view_movie():
+	return render_template('user/viewmovie.html',movie=movie)
 
+# @app.route('/bookmovie')
+# def book_movie():
+# 	return render_template('user/bookmovie.html')
 
-# @app.route('/change_movie',method=['POST'])
-# def change_movie():
+@app.route('/searchmovie')
+def search_movie():
+	return render_template('user/searchmovie.html')
 
-# @app.route('/all_movies')
-# def all_movies():
+@app.route('/allmovies')
+def all_movie():
+	movies = db.get_movies()
+	return render_template('user/allmovie.html',movies=movies)
 
+@app.route('/bookmovie/submit',methods=['POST'])
+def book_movie_sub():
+	return render_template('user/bookmovie.html')
+
+@app.route('/searchmovie/submit',methods=['POST'])
+def search_movie_sub():
+	movie = request.form['mname']
+	print(movie)
+	res = db.get_movie_search(movie)
+	if res:
+		return render_template('user/viewmovie.html',movie=res)
+	return render_template('user/searchmovie.html')
 # @app.route('/logout')
 # def logout():
 # 	session.pop('user',None)
 # 	return redirect(url_for('logged_in'))
-
-
-
-
 
 
 if __name__ == '__main__':
